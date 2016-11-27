cf081dd5efd33b8f8f31a2b92a1210b87803127b
Robert Kuykendall <robert@rkuykendall.com>
2016-11-27 14:21:54 -0500

    Update settings.py

--- wheretostartreading/settings.py
+++ wheretostartreading/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['wheretostartreading.com', 'localhost']
+ALLOWED_HOSTS = ['.wheretostartreading.com', 'localhost']
 
 
 # Application definition
