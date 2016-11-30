2a35b448c0553a9f07f323cfe6fe9a5e6aee8741
Robert Kuykendall <robert@rkuykendall.com>
2016-11-29 21:14:42 -0500

    Add sitemap

--- wheretostartreading/urls.py
+++ wheretostartreading/urls.py
@@ -1,22 +1,32 @@
-"""wheretostartreading URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import GenericSitemap, Sitemap
+from django.core.urlresolvers import reverse
+
+from blog.models import Article
+
+class HomepageSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'daily'
+
+    def items(self):
+        return ['home']
+
+    def location(self, item):
+        return reverse(item)
+
+sitemaps = {
+    'blog': GenericSitemap({
+            'queryset': Article.objects.all(),
+            'date_field': 'modified_at',
+        }, priority=0.6),
+    'homepage': HomepageSitemap,
+}
 
 urlpatterns = [
     url(r'^gauntlet/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'),
 ]
