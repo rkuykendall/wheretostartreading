f33b779cea7dc6000e7fcb4392f58c9cb551593a
Robert Kuykendall <robert@rkuykendall.com>
2016-12-04 14:51:53 -0500

    Better admin.

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
+from django import forms
+
 from .models import Article
+
+
+class ArticleModelForm(forms.ModelForm):
+    description = forms.CharField(widget=forms.Textarea)
+
+    class Meta:
+        exclude = ()
+        model = Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'published_at', 'modified_at', )
     list_filter = ('published_at', )
+    form = ArticleModelForm
 
 admin.site.register(Article, ArticleAdmin)
