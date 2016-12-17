318c4e4d4042f893506b5647f7aa9b34c63f4319
Robert Kuykendall <robert@rkuykendall.com>
2016-12-17 13:06:59 -0600

    Add short titles and similar media recommendations.

--- blog/admin.py
+++ blog/admin.py
@@ -1,20 +1,10 @@
 from django.contrib import admin
-from django import forms
 
 from .models import Article
 
 
-class ArticleModelForm(forms.ModelForm):
-    description = forms.CharField(widget=forms.Textarea)
-
-    class Meta:
-        exclude = ()
-        model = Article
-
-
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'published_at', 'modified_at', )
+    list_display = ('title', 'title_short', 'published_at', 'modified_at', )
     list_filter = ('published_at', )
-    form = ArticleModelForm
 
 admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -64,10 +64,17 @@ class Article(models.Model):
         default='markdown',
     )
 
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True, max_length=255)
-    description = models.CharField(max_length=255, null=True, blank=True)
-    content = models.TextField()
+    title = models.CharField(
+        'Title', max_length=255)
+    title_short = models.CharField(
+        'Short title', max_length=255, null=True, blank=True)
+    if_you_like = models.CharField(
+        'If you like', max_length=255, null=True, blank=True)
+    slug = models.SlugField(
+        'Slug', unique=True, max_length=255)
+    description = models.CharField(
+        'Description', max_length=160, null=True, blank=True)
+    content = models.TextField('Content')
 
     class Meta:
         ordering = ['-published_at', '-modified_at']
