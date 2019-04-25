da4450035a895120ca7e6d77f38ce0528c26bf1e
Robert Kuykendall <robert@rkuykendall.com>
2019-04-25 09:25:28 -0400

    Move CSS to static and easier editing in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.forms import Textarea
+from django.db import models
 
 from .models import Article
 
@@ -6,6 +8,10 @@ from .models import Article
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'title_short', 'published_at', 'modified_at', )
     list_filter = ('published_at', )
+
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 70, 'cols': 120})},
+    }
 
 
 admin.site.register(Article, ArticleAdmin)
